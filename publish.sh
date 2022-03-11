dd6769c85b406d9b57c495e7608129d11ee91172
Kire <kirep@vestelglobal.com>
2022-03-11 11:02:32 +0000

    Added publish script

---
+++ publish.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -e
+git add .
+echo $1
+git commit -m "$1"
+git push
+
+VERSION=$(git tag | sort -V | tail -1)
+
+PARTS=(${VERSION//./ })
+LENGTH=${#PARTS[@]}
+LAST=${PARTS[LENGTH-1]}
+INCREMENTED=$(( $LAST + 1 ))
+NEWVERSION=""
+
+for s in "${PARTS[@]}"
+do
+    :
+    if [ $s != ${PARTS[LENGTH-1]} ]
+    then
+        NEWVERSION+=$s.
+    else
+        NEWVERSION+=$INCREMENTED
+    fi
+done
+
+git tag -a $NEWVERSION -m "$NEWVERSION"
+git push --tags
