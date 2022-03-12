1bab76366b295e03fb28a88017493bb342472f44
Kire <kirepetrovv.96@hotmail.com>
2022-03-12 16:33:27 +0000

    Extra logs

--- rtpreceiver_js.go
+++ rtpreceiver_js.go
@@ -3,10 +3,18 @@
 
 package webrtc
 
-import "syscall/js"
+import (
+	"fmt"
+	"syscall/js"
+)
 
 // RTPReceiver allows an application to inspect the receipt of a TrackRemote
 type RTPReceiver struct {
 	// Pointer to the underlying JavaScript RTCRTPReceiver object.
 	underlying js.Value
+}
+
+func (r *RTPReceiver) Track() string {
+	fmt.Println("THE UNDERLYING IS ", r.underlying)
+	return r.underlying.Get("track").String()
 }
