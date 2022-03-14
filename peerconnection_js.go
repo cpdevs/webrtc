0527ff6ed6bba7e518d4cc2f8c132ff398cdf2b4
Kire <kirepetrovv.96@hotmail.com>
2022-03-14 16:34:41 +0000

    Extra logs

--- peerconnection_js.go
+++ peerconnection_js.go
@@ -117,6 +117,7 @@ func (pc *PeerConnection) OnTrack(f func(*TrackRemote, *RTPReceiver)) {
 		fmt.Println("Receiver is ", args[0].Get("receiver"))
 		fmt.Println("Streams are ", args[0].Get("streams"))
 		fmt.Println("Trying to access the 0 index of streams ", args[0].Get("streams"))
+		fmt.Println("The type of streams is ", args[0].Get("streams").Type())
 		streams := args[0].Get("streams")
 		buf := make([]byte, streams.Get("length").Int())
 		n := js.CopyBytesToGo(buf, streams)
