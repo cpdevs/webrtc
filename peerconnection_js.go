24a4c42d1a1577660266939bd4c3e63723891681
Kire <kirepetrovv.96@hotmail.com>
2022-03-15 11:04:45 +0000

    Extra logs

--- peerconnection_js.go
+++ peerconnection_js.go
@@ -125,7 +125,7 @@ func (pc *PeerConnection) OnTrack(f func(*TrackRemote, *RTPReceiver)) {
 		}
 		if t.underlying.Get("kind").String() == "video" {
 			stream := streams.Call("slice", 0, 1).Call("shift")
-			fmt.Println("Will set src object of video with stream ", stream.Get("id"))
+			fmt.Println("Will set src object of video with stream ", stream.Get("id").String())
 			js.Global().Get("document").Call("getElementById", "webrtc_video").Set("srcObject", stream)
 			js.Global().Get("document").Call("getElementById", "webrtc_video").Set("id", "kire")
 		}
